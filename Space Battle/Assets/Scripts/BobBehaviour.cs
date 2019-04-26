95392cbeb4f64a8790c8ae399b50392c86d48f1c
Connor Ryan <c16755215@mydit.ie>
2019-04-26 13:43:38 +0100

    bob added

---
+++ Space Battle/Assets/Scripts/BobBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobBehaviour : MonoBehaviour
+{
+    float frequency = 5f;
+
+    float magnitude = 0.3f;
+
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.localPosition = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+}
