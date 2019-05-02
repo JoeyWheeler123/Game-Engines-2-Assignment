ab70b3c98caa71e6770f47ac8e128a9f9a77508f
Connor Ryan <c16755215@mydit.ie>
2019-05-02 22:53:35 +0100

    camera movement on arc script

---
+++ Space Battle/Assets/Scripts/CameraArc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraArc : MonoBehaviour
+{
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += Time.deltaTime;
+
+        transform.position = ParabolaMovement.Parabola(Vector3.zero, Vector3.forward * 10f, 5f, speed / 5f);
+    }
+}
