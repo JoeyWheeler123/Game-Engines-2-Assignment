ed26bf4a14963dd14db319054af012ee6cfb35e4
Connor Ryan <c16755215@mydit.ie>
2019-05-05 19:06:19 +0100

    cannon barage

---
+++ Space Battle/Assets/Scripts/CannonShoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour
+{
+    public GameObject cannonBall;
+    //public GameObject firePos1;
+
+    public int firePower = 1000;
+
+    public float time = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FireCannon());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        GameObject clone;
+        clone = Instantiate(cannonBall, transform.position, Quaternion.identity);
+        clone.GetComponent<Rigidbody>().AddForce(transform.up * firePower, ForceMode.Impulse);
+        Debug.Log("Shoot");
+    }
+
+    IEnumerator FireCannon()
+    {
+        yield return new WaitForSeconds(time);
+
+        Fire();
+    }
+}
---
+++ Space Battle/Assets/Scripts/ScriptActivator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptActivator : MonoBehaviour
+{
+    public GameObject c1;
+    public GameObject c2;
+      public GameObject c3;
+        public GameObject c4;
+        public GameObject c5;
+        public GameObject c6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c1.GetComponent<CannonShoot>().enabled = true;
+        c2.GetComponent<CannonShoot>().enabled = true;
+        c3.GetComponent<CannonShoot>().enabled = true;
+        c4.GetComponent<CannonShoot>().enabled = true;
+        c5.GetComponent<CannonShoot>().enabled = true;
+        c6.GetComponent<CannonShoot>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
